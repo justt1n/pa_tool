d37f86d25e05c168c6a11585d8085e368e2ed038
tinnt <tinnt@airtrip-intl.com>
2024-12-02 11:44:09 +0700

    .

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
+from dotenv import load_dotenv
+
+from app.login import login
 from app.process import run
+from utils.selenium_util import SeleniumUtil
 
 
 def test():
     run()
 
+def test_login():
+    browser = SeleniumUtil()
+    login(browser)
 
 if __name__ == "__main__":
-    test()
+    load_dotenv('settings.env')
+    test_login()
