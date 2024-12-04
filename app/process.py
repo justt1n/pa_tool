9c7b6be38c6822486a1921600bbdbaa94d3e3319
thdangtr <thdangtr@gmail.com>
2024-12-04 22:49:49 +0700

    calculate change price on stock fake

--- app/process.py
+++ app/process.py
@@ -14,8 +14,10 @@ from utils.sheet_operator import (
 )
 from utils.pa_extract import extract_offer_items
 from utils.g2g_extract import g2g_extract_offer_items
+from utils.fun_extract import fun_extract_offer_items
+from utils.biji_extract import bij_lowest_price
 from model.sheet_model import G2G, Product, StockInfo, FUN, BIJ
-from model.crawl_model import G2GOfferItem, OfferItem, DeliveryTime
+from model.crawl_model import G2GOfferItem, OfferItem, DeliveryTime, FUNOfferItem
 from model.payload import PriceInfo, Row
 from model.enums import StockType
 
@@ -97,31 +99,93 @@ def identify_stock(
     return StockType.stock_fake
 
 
+def calculate_price_stock_fake(
+    gsheet: GSheet,
+    row: Row,
+    quantity: int,
+):
+    g2g_min_price = None
+    if row.g2g.G2G_CHECK == 1:
+        g2g_offer_items = g2g_extract_offer_items(row.g2g.G2G_PRODUCT_COMPARE)
+        filtered_g2g_offer_items = G2GOfferItem.filter_valid_g2g_offer_item(
+            g2g=row.g2g,
+            g2g_blacklist=row.g2g.get_blacklist(gsheet),
+            g2g_offer_items=g2g_offer_items,
+        )
+        if filtered_g2g_offer_items:
+            g2g_min_offer_item = G2GOfferItem.min_offer_item(filtered_g2g_offer_items)
+            g2g_min_price = (
+                g2g_min_offer_item.price_per_unit * quantity * row.g2g.G2G_PROFIT
+            )
+            print(f"G2G min price: {g2g_min_price}")
+
+    fun_min_price = None
+    if row.fun.FUN_CHECK == 1:
+        fun_offer_items = fun_extract_offer_items(
+            row.fun.FUN_PRODUCT_COMPARE,
+            [
+                i
+                for i in [
+                    row.fun.FUN_FILTER21,
+                    row.fun.FUN_FILTER22,
+                    row.fun.FUN_FILTER23,
+                    row.fun.FUN_FILTER24,
+                ]
+                if i is not None
+            ],
+        )
+        filtered_fun_offer_items = FUNOfferItem.filter_valid_fun_offer_items(
+            fun=row.fun,
+            fun_offer_items=fun_offer_items,
+            fun_blacklist=row.fun.get_blacklist(gsheet),
+        )
+        if filtered_fun_offer_items:
+            fun_min_offer_item = FUNOfferItem.min_offer_item(filtered_fun_offer_items)
+            fun_min_price = (
+                fun_min_offer_item.price
+                * row.fun.FUN_PROFIT
+                * row.fun.FUN_DISCOUNTFEE
+                * quantity
+            )
+            print(f"FUN min price: {fun_min_price}")
+
+        bij_min_price = None
+        if row.bij.BIJ_CHECK == 1:
+            pass
+
+    return min(
+        [i for i in [g2g_min_price, fun_min_price, bij_min_price] if i is not None]
+    )
+
+
 def calculate_price_change(
     gsheet: GSheet,
-    product: Product,
-    stock_info: StockInfo,
+    row: Row,
     offer_items: list[OfferItem],
+    BIJ_HOST_DATA: list,
 ) -> PriceInfo:
     stock_type = identify_stock(
         gsheet,
-        stock_info,
+        row.stock_info,
     )
     if stock_type is StockType.stock_1:
-        product_min_price = product.min_price_stock_1(gsheet)
-        product_max_price = product.max_price_stock_1(gsheet)
+        product_min_price = row.product.min_price_stock_1(gsheet)
+        product_max_price = row.product.max_price_stock_1(gsheet)
 
     elif stock_type is StockType.stock_2:
-        product_min_price = product.min_price_stock_2(gsheet)
-        product_max_price = product.max_price_stock_2(gsheet)
+        product_min_price = row.product.min_price_stock_2(gsheet)
+        product_max_price = row.product.max_price_stock_2(gsheet)
 
     elif stock_type is StockType.stock_fake:
-        product_min_price = 99999.0  # TODO
-        product_max_price = 99999.0  # TODO
+        stock_fake_price = calculate_price_stock_fake(
+            gsheet=gsheet, row=row, quantity=offer_items[0].quantity
+        )
+        product_min_price = stock_fake_price  # TODO
+        product_max_price = stock_fake_price  # TODO
 
     min_offer_item = OfferItem.min_offer_item(
         filter_valid_offer_items(
-            product,
+            row.product,
             offer_items,
         )
     )
@@ -131,15 +195,17 @@ def calculate_price_change(
     elif min_offer_item.price > product_max_price:  # type: ignore
         adjusted_price = product_max_price
     else:
-        range_adjust = random.uniform(product.DONGIAGIAM_MIN, product.DONGIAGIAM_MAX)
+        range_adjust = random.uniform(
+            row.product.DONGIAGIAM_MIN, row.product.DONGIAGIAM_MAX
+        )
         adjusted_price = round(
             min_offer_item.price - range_adjust,  # type: ignore
-            product.DONGIA_LAMTRON,
+            row.product.DONGIA_LAMTRON,
         )
     return PriceInfo(
         price_min=product_min_price,
         price_mac=product_max_price,
-        adjusted_price=adjusted_price,  # type: ignore
+        adjusted_price=round(adjusted_price, row.product.DONGIA_LAMTRON),  # type: ignore
         offer_item=min_offer_item,
         stock_type=stock_type,
         range_adjust=range_adjust,
@@ -170,10 +236,9 @@ def run():
     for index in row_indexes:
         row = Row.from_row_index(worksheet, index)
         offer_items = extract_offer_items(row.product.PRODUCT_COMPARE)
-        # if is_change_price(row.product, offer_items):
-        #     print(
-        #         calculate_price_change(
-        #             gsheet, row.product, row.stock_info, offer_items
-        #         ).model_dump(mode="json")
-        #     )
-        print(bij_lowest_price(gsheet, row.g2g))
+        if is_change_price(row.product, offer_items):
+            print(
+                calculate_price_change(
+                    gsheet, row.product, row.stock_info, offer_items
+                ).model_dump(mode="json")
+            )
