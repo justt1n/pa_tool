1f4d2b1be8aed13aa52a70ca90c982dd152a2794
justt1n <tinnt0709@gmail.com>
2025-01-14 21:15:59 +0700

    add min max stock fake to log

--- main.py
+++ main.py
@@ -266,7 +266,7 @@ def get_update_str(offer_item: OfferItem, item_info: PriceInfo, stock_fake_items
         _str += f"PriceMin = {item_info.price_min}, PriceMax = {item_info.price_mac},\n"
         return _str + "\n"
     if stock_fake_items:
-        _str += f"PriceMin = stock fake, PriceMax = stock fake, \n"
+        _str += f"PriceMin = {item_info.price_min}, PriceMax = {item_info.price_mac}, \n"
         if stock_fake_items[0] is not None:
             _str += f"\nMin G2G: {stock_fake_items[0][1]} = {stock_fake_items[0][0]}, \n"
         else:
