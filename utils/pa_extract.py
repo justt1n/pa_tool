1978c1ee0611646e42cf04e358df96780fc21556
justt1n <tinnt0709@gmail.com>
2025-04-08 18:42:24 +0700

    fix site blocking

--- utils/pa_extract.py
+++ utils/pa_extract.py
@@ -14,7 +14,14 @@ from requests import HTTPError
 def __get_soup(
         url: str,
 ) -> BeautifulSoup:
-    res = requests.get(url=url)
+    headers_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
+    ]
+    random_agent_from_list = headers_agents[int(random.uniform(0, len(headers_agents)))]
+    res = requests.get(url, headers={"User-Agent": random_agent_from_list})
     # res.raise_for_status()
     return BeautifulSoup(res.text, "html.parser")
 
